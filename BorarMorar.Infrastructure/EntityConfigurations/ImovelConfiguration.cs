b2e59f5ee6e2bde34cfe148dcadb6f45ee6b1b9f
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-04-30 13:25:01 -0300

    Criação da Infra Layer

---
+++ BorarMorar.Infrastructure/EntityConfigurations/ImovelConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using BoraMorar.Imoveis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace BoraMorar.Infrastructure.EntityConfigurations;
+
+public class ImovelConfiguration : IEntityTypeConfiguration<Imovel>
+{
+    public void Configure(EntityTypeBuilder<Imovel> builder)
+    {
+        builder.HasKey(o => o.Id);
+    }
+}
---
+++ BorarMorar.Infrastructure/EntityConfigurations/IndiceAplicadoConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using BoraMorar.Indices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BoraMorar.Infrastructure.EntityConfigurations;
+
+public class IndiceAplicadoConfiguration : IEntityTypeConfiguration<IndiceAplicado>
+{
+    public void Configure(EntityTypeBuilder<IndiceAplicado> builder)
+    {
+        builder.HasKey(o => o.Id);
+    }
+}
