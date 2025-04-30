970ee51ccecd1c0c70631bf6a649a7b503acfe01
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-04-30 13:25:01 -0300

    Criação da Infra Layer

---
+++ BorarMorar.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using BoraMorar.Imoveis.Repository;
+using BoraMorar.Infrastructure.Repositories;
+using BoraMorar.Infrastructure;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext();
+        services.AddRepositories();
+        services.AddOpenTelemetryExporter();
+    }
+    private static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IImovelRepository, ImovelRepository>();
+    }
+    private static void AddDbContext(this IServiceCollection services)
+    {
+        services.AddDbContext<BoraMorarDbContext>(options => options.UseInMemoryDatabase(nameof(BoraMorarDbContext)));
+    }
+
+    private static void AddOpenTelemetryExporter(this IServiceCollection services)
+    {
+        var assemblyName = Assembly.GetEntryAssembly()?.GetName();
+        var serviceName = assemblyName?.Name ?? "Unknown Service Name";
+        var serviceVersion = assemblyName?.Version?.ToString() ?? "Unknown Version";
+
+        services.AddOpenTelemetry()
+            .WithTracing(tracerBuilder =>
+            {
+                tracerBuilder
+                    .ConfigureResource(rb => rb.AddService(serviceName, null, serviceVersion))
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddOtlpExporter();
+            })
+            .WithMetrics(meterBuilder =>
+            {
+                meterBuilder
+                    .ConfigureResource(rb => rb.AddService(serviceName, null, serviceVersion))
+                    .AddRuntimeInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .AddOtlpExporter();
+            })
+            .WithLogging(loggingBuilder =>
+            {
+                loggingBuilder
+                    .ConfigureResource(rb => rb.AddService(serviceName, null, serviceVersion))
+                    .AddOtlpExporter();
+            });
+    }
+}
---
+++ BorarMorar.Infrastructure/Repositories/IndiceAplicadoRepository.cs
@@ -0,0 +1,19 @@
+﻿using BoraMorar.Indices;
+using BoraMorar.Indices.Repository;
+
+namespace BoraMorar.Infrastructure.Repositories;
+
+public class IndiceAplicadoRepository(BoraMorarDbContext dbContext) : IIndiceAplicadoRepository
+{
+    public ICommitScope CommitScope => dbContext;
+
+    public void Add(IndiceAplicado indice)
+    {
+        dbContext.Add(indice);
+    }
+
+    public async Task<IndiceAplicado?> FindAsync(int id)
+    {
+        return await dbContext.FindAsync<IndiceAplicado>(id);
+    }
+}
