a55ef5f13a07ddac70f5b77cce5957d5f2948197
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-04-30 13:25:01 -0300

    Criação da Infra Layer

---
+++ BorarMorar.Infrastructure/BorarMorarDbContext.cs
@@ -0,0 +1,37 @@
+﻿using CSharpFunctionalExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace BoraMorar.Infrastructure;
+
+public class BoraMorarDbContext(DbContextOptions options, IMediator mediator) : DbContext(options), ICommitScope
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var thisAssembly = Assembly.GetExecutingAssembly();
+        modelBuilder.ApplyConfigurationsFromAssembly(thisAssembly);
+    }
+
+    public async Task<int> CommitAsync(CancellationToken cancellationToken = default)
+    {
+        var domainEntities = ChangeTracker.Entries<AggregateRoot>()
+            .Where(x => x.Entity.DomainEvents.Any())
+            .ToList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.Entity.DomainEvents)
+            .ToList();
+
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        foreach (var domainEvent in domainEvents)
+            await mediator.Publish(domainEvent, cancellationToken);
+
+        domainEntities.ForEach(entity => entity.Entity.ClearDomainEvents());
+
+        return result;
+    }
+
+    public int Commit(CancellationToken cancellationToken = default) => CommitAsync(cancellationToken).Result;
+}
---
+++ BorarMorar.Infrastructure/Repositories/ImovelRepository.cs
@@ -0,0 +1,19 @@
+﻿using BoraMorar.Imoveis;
+using BoraMorar.Imoveis.Repository;
+
+namespace BoraMorar.Infrastructure.Repositories;
+
+public class ImovelRepository(BoraMorarDbContext dbContext) : IImovelRepository
+{
+    public ICommitScope CommitScope => dbContext;
+
+    public void Add(Imovel imovel)
+    {
+        dbContext.Add(imovel);
+    }
+
+    public async Task<Imovel?> FindAsync(int id)
+    {
+        return await dbContext.FindAsync<Imovel>(id);
+    }
+}
