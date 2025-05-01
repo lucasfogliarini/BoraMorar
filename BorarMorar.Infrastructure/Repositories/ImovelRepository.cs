5e1f719d2e002e8155fdd11137012f220694412b
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-04-30 23:46:37 -0300

    add AddRange

--- BorarMorar.Infrastructure/Repositories/ImovelRepository.cs
+++ BorarMorar.Infrastructure/Repositories/ImovelRepository.cs
@@ -12,6 +12,11 @@ public class ImovelRepository(BoraMorarDbContext dbContext) : IImovelRepository
         dbContext.Add(imovel);
     }
 
+    public void AddRange(IEnumerable<Imovel> imoveis)
+    {
+        dbContext.AddRange(imoveis);
+    }
+
     public async Task<Imovel?> FindAsync(int id)
     {
         return await dbContext.FindAsync<Imovel>(id);
