00af38a81991520906eb15411479ffff8b08cea4
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-14 20:20:09 -0300

    create Application Layer

---
+++ BoraMorar.Application/ICommand.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application;
+
+/// <summary>
+/// Representa um comando que expressa uma intenção de ação no domínio, sem retorno.
+/// Cor no EventStorming: <b>Azul</b> (Command).
+/// </summary>
+public interface ICommand;
+
+/// <summary>
+/// Representa um comando que expressa uma intenção de ação no domínio e espera uma resposta.
+/// Cor no EventStorming: <b>Azul</b> (Command).
+/// </summary>
+public interface ICommand<TResponse>;
+
+public interface ICommandHandler<in TCommand> where TCommand : ICommand
+{
+    Task<Result> Handle(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand, TResponse> where TCommand : ICommand<TResponse>
+{
+    Task<Result<TResponse>> Handle(TCommand command, CancellationToken cancellationToken);
+}
---
+++ BoraMorar.Application/IQuery.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application;
+
+/// <summary>
+/// Representa uma consulta ao sistema que retorna uma resposta.
+/// Cor no EventStorming: <b>Verde</b> (Read Model / Query).
+/// </summary>
+public interface IQuery<TResponse>;
+
+public interface IQueryHandler<in TQuery, TResponse> where TQuery : IQuery<TResponse>
+{
+    Task<Result<TResponse>> Handle(TQuery query, CancellationToken cancellationToken);
+}
