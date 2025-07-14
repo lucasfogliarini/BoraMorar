61c35bbc2fc4939dbf6ab7a572e654d66e059714
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-14 20:20:09 -0300

    create Application Layer

---
+++ BoraMorar.Application/Imoveis/Buscar/BuscarImovelCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using BoraMorar.Imoveis;
+using BoraMorar.Imoveis.Repository;
+using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application.Imoveis.Buscar;
+
+internal sealed class BuscarImovelCommandHandler(IImovelRepository repository) : ICommandHandler<BuscarImovelCommand, BuscarImovelResponse>
+{
+    public async Task<Result<BuscarImovelResponse>> Handle(BuscarImovelCommand command, CancellationToken cancellationToken)
+    {
+        Result<Imovel> result = await repository.FindAsync(command.Id);
+        return result
+            .EnsureNotNull("Imóvel não encontrado.")
+            .MapTry(c => new BuscarImovelResponse(c.Id));
+    }
+}
