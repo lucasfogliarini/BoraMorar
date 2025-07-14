b85d4aed437927a9a5de3e3e8a1eb87be4cbbd57
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-14 19:04:16 -0300

    add IDomainEventHandler

--- BoraMorar/IDomainEvent.cs
+++ BoraMorar/IDomainEvent.cs
@@ -1,4 +1,9 @@
 ﻿namespace BoraMorar;
 
+public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
+{
+    Task HandleAsync(TEvent domainEvent, CancellationToken cancellationToken);
+}
+
 public interface IDomainEvent;
 
