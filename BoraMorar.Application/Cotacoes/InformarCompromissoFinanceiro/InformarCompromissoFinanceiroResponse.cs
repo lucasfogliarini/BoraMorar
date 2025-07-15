3b4f7799f98874f130e42bf5d8d4aaf4c1334070
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:20:35 -0300

    add InformarCompromissoFinanceiro

---
+++ BoraMorar.Application/Cotacoes/InformarCompromissoFinanceiro/InformarCompromissoFinanceiroResponse.cs
@@ -0,0 +1,5 @@
+﻿using BoraMorar.Cotacoes;
+
+namespace BoraMorar.Application.Cotacoes.InformarCompromissoFinanceiro;
+
+public record InformarCompromissoFinanceiroResponse(int Id, CotacaoStatus Status, DateTime DataCompromissoFinanceiroInformado);
