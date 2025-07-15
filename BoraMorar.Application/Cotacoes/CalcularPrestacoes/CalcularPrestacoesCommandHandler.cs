b716de77f724f27351a628c38626995f70da0ac0
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:25:50 -0300

    add CalcularPrestacoes

---
+++ BoraMorar.Application/Cotacoes/CalcularPrestacoes/CalcularPrestacoesCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using BoraMorar.Cotacoes;
+using BoraMorar.Cotacoes.Repository;
+using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application.Cotacoes.CalcularPrestacoes;
+
+public class CalcularPrestacoesCommandHandler(ICotacaoRepository repository) : ICommandHandler<CalcularPrestacoesCommand, CalcularPrestacoesResponse>
+{
+    public async Task<Result<CalcularPrestacoesResponse>> Handle(CalcularPrestacoesCommand command, CancellationToken cancellationToken = default)
+    {
+        Result<Cotacao> result = await repository.FindAsync(command.Id);
+        return result
+            .EnsureNotNull("Cotação não encontrada.")
+            .Tap(c =>
+            {
+                c.CalcularPrestacoes(command.TaxaJuros, command.PrazoMaximo);
+                repository.CommitScope.Commit(cancellationToken);
+            })
+            .MapTry(c => new CalcularPrestacoesResponse(c.Id, c.Status, c.DataPrestacoesCalculadas, c.PrestacaoPrazoPretendido, c.PrestacaoPrazoMaximo));
+    }
+}
+
+public record CalcularPrestacoesCommand(int Id, decimal TaxaJuros, int PrazoMaximo) : ICommand<CalcularPrestacoesResponse>;
+
+public record CalcularPrestacoesResponse(int Id, CotacaoStatus Status, DateTime DataPrestacoesCalculadas, decimal PrestacaoPrazoPretendido, decimal PrestacaoPrazoMaximo);
