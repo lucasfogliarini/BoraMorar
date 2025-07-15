bbba73fbde87fbdfdb02c9273c6f8d036e9167e1
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:33:30 -0300

    add BuscarCotacaoCommandHandler

---
+++ BoraMorar.Application/Cotacoes/BuscarCotacao/BuscarCotacaoCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using BoraMorar.Cotacoes.Repository;
+using CSharpFunctionalExtensions;
+using CSharpFunctionalExtensions.ValueTasks;
+
+namespace BoraMorar.Application.Cotacoes.BuscarCotacao;
+
+public class BuscarCotacaoCommandHandler(ICotacaoRepository repository) : IQueryHandler<BuscarCotacaoQuery, BuscarCotacaoResponse>
+{
+    public async Task<Result<BuscarCotacaoResponse>> Handle(BuscarCotacaoQuery query, CancellationToken cancellationToken = default)
+    {
+        return await Result
+            .Try(() => repository.FindAsync(query.Id))
+            .EnsureNotNull("Cotação não encontrada.")
+            .MapTry(c => new BuscarCotacaoResponse(c.Id, c.Numero, c.DataCotacaoSolicitada));
+    }
+}
+
+public record BuscarCotacaoQuery(int Id, decimal TaxaJuros, int PrazoMaximo) : IQuery<BuscarCotacaoResponse>;
+
+public record BuscarCotacaoResponse(int Id, string Numero, DateTime DataCotacaoSolicitada);
