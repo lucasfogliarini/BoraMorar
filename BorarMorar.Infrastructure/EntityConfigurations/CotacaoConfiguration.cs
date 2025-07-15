90afcb2230720d555fb4d7cac627f98ebd285f63
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 19:48:20 -0300

    add CotacaoRepository

---
+++ BorarMorar.Infrastructure/EntityConfigurations/CotacaoConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace BoraMorar.Infrastructure.EntityConfigurations;
+
+public class CotacaoConfiguration : IEntityTypeConfiguration<Cotacao>
+{
+    public void Configure(EntityTypeBuilder<Cotacao> builder)
+    {
+        builder.HasKey(o => o.Id);
+    }
+}
