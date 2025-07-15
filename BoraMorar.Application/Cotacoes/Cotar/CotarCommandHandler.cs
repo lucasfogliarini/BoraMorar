b3a8da1af38228ba14e2f11f8b2ba08cfc99a96f
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:03:48 -0300

    Create CotarCommand

---
+++ BoraMorar.Application/Cotacoes/Cotar/CotarCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using BoraMorar.Cotacoes.Repository;
+using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application.Cotacoes.Cotar;
+
+public class CotarCommandHandler(ICotacaoRepository repository) : ICommandHandler<CotarCommand, CotarResponse>
+{
+    public async Task<Result<CotarResponse>> Handle(CotarCommand command, CancellationToken cancellationToken = default)
+    {
+        return await Result
+            .Try(async () =>
+            {
+                var cotacao = new Cotacao(command.ClienteId, command.TipoDoBem, command.Preco);
+                repository.Add(cotacao);
+                await repository.CommitScope.CommitAsync(cancellationToken);
+
+                return new CotarResponse(cotacao.Id, cotacao.Numero, cotacao.Status, cotacao.DataCotacaoSolicitada);
+            });
+    }
+}
