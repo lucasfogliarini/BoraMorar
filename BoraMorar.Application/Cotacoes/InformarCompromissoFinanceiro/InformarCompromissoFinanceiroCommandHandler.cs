6adae992bf2b0ce138423662c8e068f4b8dfa794
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:20:35 -0300

    add InformarCompromissoFinanceiro

---
+++ BoraMorar.Application/Cotacoes/InformarCompromissoFinanceiro/InformarCompromissoFinanceiroCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using BoraMorar.Cotacoes.Repository;
+using CSharpFunctionalExtensions;
+
+namespace BoraMorar.Application.Cotacoes.InformarCompromissoFinanceiro;
+
+public class InformarCompromissoFinanceiroCommandHandler(ICotacaoRepository repository) : ICommandHandler<InformarCompromissoFinanceiroCommand, InformarCompromissoFinanceiroResponse>
+{
+    public async Task<Result<InformarCompromissoFinanceiroResponse>> Handle(InformarCompromissoFinanceiroCommand command, CancellationToken cancellationToken = default)
+    {
+        Result<Cotacao> result = await repository.FindAsync(command.Id);
+        return result
+            .EnsureNotNull("Cotação não encontrada.")
+            .Tap(c =>
+            {
+                c.InformarCompromissoFinanceiro(command.RendaBrutaMensal, command.PrazoPretendido);
+                repository.CommitScope.Commit();
+            })
+            .MapTry(c => new InformarCompromissoFinanceiroResponse(c.Id, c.Status, c.DataCompromissoFinanceiroInformado));
+    }
+}
