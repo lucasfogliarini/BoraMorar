bb3bacd4cd7f527af0cf062914bc66bc8ad5c82c
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 20:15:53 -0300

    add SolicitarRendaHandler

---
+++ BoraMorar.Application/Cotacoes/SolicitarRenda/SolicitarRendaResponse.cs
@@ -0,0 +1,5 @@
+﻿using BoraMorar.Cotacoes;
+
+namespace BoraMorar.Application.Cotacoes.SolicitarRenda;
+
+public record SolicitarRendaResponse(int Id, CotacaoStatus Status, DateTime DataRendaSolicitada);
