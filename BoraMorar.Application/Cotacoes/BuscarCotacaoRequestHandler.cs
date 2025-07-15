977952d573252afb0637118205361b088e04f0c3
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 19:44:27 -0300

    Criação das Cotacoes

---
+++ BoraMorar.Application/Cotacoes/BuscarCotacaoRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using BoraCotacoes.Propostas.Repository;
+using CSharpFunctionalExtensions;
+using MediatR;
+
+namespace BoraCotacoes.RequestHandlers;
+
+public class BuscarCotacaoRequestHandler(ICotacaoRepository repository) : IRequestHandler<BuscarCotacaoRequest, Result<BuscarCotacaoResponse>>
+{
+    public async Task<Result<BuscarCotacaoResponse>> Handle(BuscarCotacaoRequest request, CancellationToken cancellationToken)
+    {
+        Result<Cotacao> result = await repository.FindAsync(request.Id);
+        return result
+            .EnsureNotNull("Cotação não encontrada.")
+            .MapTry(c => new BuscarCotacaoResponse(c.Id, c.Numero, c.DataCotacaoSolicitada));
+    }
+}
+
+public record BuscarCotacaoRequest(int Id) : IRequest<Result<BuscarCotacaoResponse>>;
+
+public record BuscarCotacaoResponse(int Id, string Numero, DateTime DataCotacaoSolicitada);
