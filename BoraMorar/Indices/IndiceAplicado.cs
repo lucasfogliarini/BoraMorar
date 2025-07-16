d31eca309759f497c891016286d322e6817fc0ae
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 22:06:21 -0300

    ajuste no cost de IndiceAplicado

--- BoraMorar/Indices/IndiceAplicado.cs
+++ BoraMorar/Indices/IndiceAplicado.cs
@@ -3,11 +3,23 @@
     /// <summary>
     /// Representa o valor de um índice aplicado a um endereço em uma data de referência.
     /// </summary>
-    public class IndiceAplicado(Indice indice, Endereco endereco, decimal valor, DateOnly dataReferencia) : AggregateRoot
+    public class IndiceAplicado : AggregateRoot
     {
-        public DateOnly DataReferencia { get; private set; } = dataReferencia;
-        public Indice Indice { get; private set; } = indice;
-        public Endereco Endereco { get; private set; } = endereco;
-        public decimal Valor { get; private set; } = valor;
+        public IndiceAplicado(Indice indice, Endereco endereco, decimal valor, DateOnly dataReferencia)
+        {
+            DataReferencia = dataReferencia;
+            Indice = indice;
+            Endereco = endereco;
+            Valor = valor;
+        }
+
+        public IndiceAplicado()
+        {
+        }
+
+        public DateOnly DataReferencia { get; private set; }
+        public Indice Indice { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public decimal Valor { get; private set; }
     }
 }
