ba0f7a0c2b49852f3fa76e3447a5b91aaaa845bf
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 21:03:40 -0300

    add CotarRequest

--- BoraMorar.Functions/CotacaoStatusChangedConsumer.cs
+++ BoraMorar.Functions/CotacaoStatusChangedConsumer.cs
@@ -1,25 +1,20 @@
-using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.WebJobs.Extensions.Kafka;
-using MediatR;
-using BoraCotacoes.Cotacoes.DomainEvents;
-using BoraCotacoes.RequestHandlers;
-using BoraCotacoes.Cotacoes;
 
-namespace BoraCotacoes.Consumers
+namespace BoraMorar.Functions;
+
+public class CotacaoStatusChangedConsumer(IMediator mediator)
 {
-    public class CotacaoStatusChangedConsumer(IMediator mediator)
+    [Function(nameof(CotacaoStatusChangedConsumer))]
+    public void Run(
+        [KafkaTrigger("%BrokerList%",
+                      "cotacao-status-changed",
+                      ConsumerGroup = nameof(CotacaoStatusChangedConsumer))]
+                      CotacaoStatusChangedDomainEvent domainEvent)
     {
-        [FunctionName(nameof(CotacaoStatusChangedConsumer))]
-        public void Run(
-            [KafkaTrigger("%BrokerList%",
-                          "cotacao-status-changed",
-                          ConsumerGroup = nameof(CotacaoStatusChangedConsumer))]
-                          CotacaoStatusChangedDomainEvent domainEvent)
-        {
-            if(domainEvent.Status != CotacaoStatus.CotacaoAprovada)
-                return;
-            var gerarProposta = new GerarPropostaRequest(domainEvent.Id);
-            mediator.Send(gerarProposta);
-        }
+        if(domainEvent.Status != CotacaoStatus.CotacaoAprovada)
+            return;
+        var gerarProposta = new GerarPropostaRequest(domainEvent.Id);
+        mediator.Send(gerarProposta);
     }
 }
