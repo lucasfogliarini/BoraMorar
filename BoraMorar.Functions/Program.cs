986ba40f8af70618f75678215899b278a8dac3c8
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 21:03:40 -0300

    add CotarRequest

--- BoraMorar.Functions/Program.cs
+++ BoraMorar.Functions/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -7,8 +6,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+builder.Services.AddInfrastructure();
+builder.Services.AddApplication();
 
 builder.Build().Run();
