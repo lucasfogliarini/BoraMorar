d86b8ab03f12fe5511e335a94debccdd06ca9bc8
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 22:29:52 -0300

    add CotacaoStatusChangedDomainEventHandler

---
+++ BoraMorar.Application/Cotacoes/CotacaoStatusChangedDomainEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using BoraMorar.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace BoraMorar.Cotacoes.DomainEvents;
+
+public class CotacaoStatusChangedDomainEventHandler(IProducer producer, ILogger<CotacaoStatusChangedDomainEventHandler> logger) : IDomainEventHandler<CotacaoStatusChangedDomainEvent>
+{
+    public async Task HandleAsync(CotacaoStatusChangedDomainEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("{id}, {status}, {occurredOn}", domainEvent.CotacaoId, domainEvent.NewStatus, domainEvent.OccurredOn);
+        await producer.ProduceAsync("cotacao-status-changed", domainEvent);
+    }
+}
+
+
--- BoraMorar.Functions/CotacaoStatusChangedConsumer.cs
+++ BoraMorar.Functions/CotacaoStatusChangedConsumer.cs
@@ -1,20 +1,22 @@
-using Microsoft.Azure.Functions.Worker;
+using BoraMorar.Cotacoes;
+using BoraMorar.Cotacoes.DomainEvents;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Kafka;
+using Microsoft.Extensions.Logging;
 
 namespace BoraMorar.Functions;
 
-public class CotacaoStatusChangedConsumer(IMediator mediator)
+public class CotacaoStatusChangedConsumer(ILogger<CotacaoStatusChangedConsumer> logger)
 {
-    [Function(nameof(CotacaoStatusChangedConsumer))]
+    [FunctionName(nameof(CotacaoStatusChangedConsumer))]
     public void Run(
         [KafkaTrigger("%BrokerList%",
                       "cotacao-status-changed",
                       ConsumerGroup = nameof(CotacaoStatusChangedConsumer))]
                       CotacaoStatusChangedDomainEvent domainEvent)
     {
-        if(domainEvent.Status != CotacaoStatus.CotacaoAprovada)
+        logger.LogInformation("Received domain event: {id}, {status}, {occurredOn}", domainEvent.CotacaoId, domainEvent.NewStatus, domainEvent.OccurredOn);
+        if (domainEvent.NewStatus != CotacaoStatus.CotacaoAprovada)
             return;
-        var gerarProposta = new GerarPropostaRequest(domainEvent.Id);
-        mediator.Send(gerarProposta);
     }
 }
