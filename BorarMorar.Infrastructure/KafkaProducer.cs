8ae5d8ff8e46df87af9a9c7899bd3939c78d8da0
Lucas Fogliarini Pedroso <lucasfogliarini@gmail.com>
2025-07-15 22:31:33 -0300

    add KafkaProducer

--- BorarMorar.Infrastructure/KafkaProducer.cs
+++ BorarMorar.Infrastructure/KafkaProducer.cs
@@ -4,34 +4,38 @@ using Confluent.Kafka;
 namespace BoraMorar.Infrastructure;
 internal class KafkaProducer : IProducer
 {
-    private readonly IProducer<Null, string> _producer;
-
-    public KafkaProducer(string bootstrapServers)
+    public class KafkaProducer : IProducer
     {
-        var config = new ProducerConfig
-        {
-            BootstrapServers = bootstrapServers
-        };
+        private readonly IProducer<Null, string> _producer;
 
-        _producer = new ProducerBuilder<Null, string>(config).Build();
+        public KafkaProducer(string bootstrapServers)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = bootstrapServers
+            };
+
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task ProduceAsync<T>(string topic, T message)
+        {
+            var json = JsonSerializer.Serialize(message);
+
+            var kafkaMessage = new Message<Null, string>
+            {
+                Value = json
+            };
+
+            try
+            {
+                var result = await _producer.ProduceAsync(topic, kafkaMessage);
+            }
+            catch (ProduceException<Null, string> ex)
+            {
+                Console.WriteLine($"❌ Falha ao enviar mensagem: {ex.Error.Reason}");
+            }
+        }
     }
 
-    public async Task ProduceAsync<T>(string topic, T message)
-    {
-        var json = JsonSerializer.Serialize(message);
-
-        var kafkaMessage = new Message<Null, string>
-        {
-            Value = json
-        };
-
-        try
-        {
-            var result = await _producer.ProduceAsync(topic, kafkaMessage);
-        }
-        catch (ProduceException<Null, string> ex)
-        {
-            Console.WriteLine($"❌ Falha ao enviar mensagem: {ex.Error.Reason}");
-        }
-    }
 }
